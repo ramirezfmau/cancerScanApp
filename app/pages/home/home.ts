3696c116520e3321aedfb8114370faf690993b8a
Mauricio Ramirez <mauricioramirez@konradgroup.com>
2016-10-05 17:41:11 -0600

    Adding select picture plugin

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,19 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
 import { Camera } from 'ionic-native';
+import { ImagePicker } from 'ionic-native';
+import { Cordova } from 'ionic-native';
 
-declare var cordova;
+
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
 
   public image: any;
+  public permissions: any;
 
-  constructor(public navCtrl: NavController ) {
+  constructor(public navCtrl: NavController, private platform: Platform) {
+
+    this.platform = platform;
+     platform.ready().then(() => {
+        this.permissions = cordova.plugins;
+        console.log(this.permissions);
+
+     });
   }
 
+  checkPermissionCallback(status):  void {
+    if(!status.hasPermission) {
+      var errorCallback = function() {
+        console.warn('Camera permission is not turned on');
+      }
+
+      this.permissions.requestPermission(
+        this.permissions.CAMERA,
+        function(status) {
+          if(!status.hasPermission) errorCallback();
+        },
+        errorCallback);
+    }
+  }
+
+
   takePicture() : void {
+    this.permissions.hasPermission(this.permissions.CAMERA, this.checkPermissionCallback, null);
     var options = {
       quality: 100,
       destinationType: Camera.DestinationType.DATA_URL,
@@ -30,16 +57,20 @@ export class HomePage {
     }, (err) => {
     // Handle error
     });
+
+
+
   }
 
   selectPicture() : void {
     var options = {
       quality: 100,
-      destinationType: Camera.DestinationType.FILE_URI,
-      sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+      maximumImagesCount: 1
     };
-
-
+    ImagePicker.getPictures(options).then((results) => {
+      this.image = results[0];
+    }, (err) => { });
+/*
     Camera.getPicture(options).then((imageURI) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
@@ -50,16 +81,18 @@ export class HomePage {
           this.image = imageURI;
         }*/
 
-      cordova.window.resolveLocalFileSystemURI(imageURI, (fileEntry) => {
 
-        this.image = fileEntry.nativeURL;
-        alert();
-       });
+         /* window.resolveLocalFileSystemURI(imageURI, (fileEntry) => {
+             alert();
+            this.image = fileEntry.nativeURL;
+
+          });*/
+
       //alert(this.image);
-    }, (err) => {
+   /* }, (err) => {
 
      // Handle error
-    });
+    });*/
   }
 
 }
